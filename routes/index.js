cc5e0754730545e130a6322b36197d7a08d2a158
Matias <Micano1992@gmail.com>
2022-03-07 23:16:11 -0300

    Primera entrega proyecto

--- routes/index.js
+++ routes/index.js
@@ -13,6 +13,6 @@ const router = express.Router()
 //Routes
 router.use('/productos', productosRoutes)
 router.use('/carrito', carritoRoutes)
-router.get('/*', (req, res) => res.status(400).json({ succes: false, error: `Ruta ${req.url} en método ${req.method} no autorizada` }));
+router.get('/*', (req, res) => res.status(400).json({ succes: false, error: `Ruta ${req.url} en método ${req.method} no implementada` }));
 
 module.exports = router;
